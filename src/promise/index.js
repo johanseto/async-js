2db96e8b41e88d0819e6a39ca7f135eaf796479e
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-08-28 12:41:56 -0500

    First promises, then meanwhile.

---
+++ src/promise/index.js
@@ -0,0 +1,41 @@
+const sometimeWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('hey we did it')
+        } else {
+            reject('Whooops!')
+        }
+    })
+}
+
+sometimeWillHappen()
+    .then(response => console.log(response))
+    .catch(err => console.err(err))
+
+const  sometimeWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout ( () => {
+            resolve('hey we did it for 2')
+        }, 2000)
+        } else {
+            const error = new Error('Whooops!')
+            reject (error)
+        }
+    })
+}
+
+sometimeWillHappen2()
+    .then(response => console.log(response))
+    .then(console.log('managing..'))
+    .catch(err => console.err(err))
+
+// all prommises logic
+
+Promise.all([sometimeWillHappen(),sometimeWillHappen2()])
+    .then(response => {
+        console.log('ARray or results',response)
+    })
+    .catch(err => {
+        console.error(err)
+    })
